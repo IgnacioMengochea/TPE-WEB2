424a6c17372a0647cac8f69c27b84abb2c551281
Ignacio Mengochea <142363950+IgnacioMengochea@users.noreply.github.com>
2025-10-28 01:19:48 -0300

    Refactor AuthHelper class and remove comments

--- TPE/helpers/AuthHelper.php
+++ TPE/helpers/AuthHelper.php
@@ -2,18 +2,12 @@
 
 class AuthHelper {
 
-    /**
-     * Verifica que el usuario esté logueado.
-     * Si no está logueado, lo redirige al login.
-     */
     public static function checkLoggedIn() {
-        // session_start() se ejecuta en el index.php,
-        // así que podemos usar $_SESSION directamente.
-        
+
         if (!isset($_SESSION['IS_LOGGED'])) {
-            // ¡No está logueado! Lo mandamos al login
             header('Location: ' . BASE_URL . 'login');
-            die(); // Detenemos la ejecución
+            die(); 
         }
     }
+
 }
