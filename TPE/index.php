07d54230b0eb2a8c83801793502cadba4374f407
Ignacio Mengochea <142363950+IgnacioMengochea@users.noreply.github.com>
2025-10-28 01:14:37 -0300

    Refactor index.php for improved routing logic

--- TPE/index.php
+++ TPE/index.php
@@ -1,29 +1,29 @@
 <?php
-// Muestra errores (SOLO PARA DESARROLLO, SACAR EN PRODUCCIÓN)
+
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-// Inicia la sesión (¡UNA SOLA VEZ!)
+
 session_start();
 
-// Incluye archivos necesarios (¡UNA SOLA VEZ!)
+
 require_once 'config.php';
 require_once 'helpers/AuthHelper.php';
 
-// Define la acción por defecto
+
 define('DEFAULT_ACTION', 'home');
 
-// Obtiene la acción de la URL
+
 if (empty($_GET['action'])) {
     $action = DEFAULT_ACTION;
 } else {
     $action = $_GET['action'];
 }
 
-// Parsea la acción
+
 $params = explode('/', $action);
 
-// Router principal
+
 switch ($params[0]) {
 
     case 'home':
@@ -64,9 +64,8 @@ switch ($params[0]) {
         break;
 
     case 'admin':
-        AuthHelper::checkLoggedIn(); // Patovica
+        AuthHelper::checkLoggedIn(); 
 
-        // Decide qué controlador de admin usar
         if (isset($params[1])) {
             // Rutas de Items (Pizzas)
             if (in_array($params[1], ['dashboard', 'agregar-form', 'agregar', 'borrar-pizza', 'editar-pizza', 'guardar-edicion'])) {
@@ -81,7 +80,7 @@ switch ($params[0]) {
                     case 'guardar-edicion': if (isset($params[2])) { $id = $params[2]; $controller->processEditPizza($id); } else { echo "Error: Falta ID."; } break;
                 }
             }
-            // Rutas de Categorías
+
             elseif (in_array($params[1], ['categorias', 'agregar-categoria-form', 'agregar-categoria', 'borrar-categoria', 'editar-categoria', 'guardar-categoria'])) {
                 require_once 'app/controllers/CategoryController.php';
                 $controller = new CategoryController();
@@ -94,20 +93,21 @@ switch ($params[0]) {
                     case 'guardar-categoria': if (isset($params[2])) { $id = $params[2]; $controller->processEditCategory($id); } else { echo "Error: Falta ID."; } break;
                 }
             }
-            // Si no coincide con nada de admin
+
             else {
                 echo "Error 404 - Página no encontrada (Admin)";
             }
         } else {
-            // Si solo es "admin/", va al dashboard de Items
+
             require_once 'app/controllers/ItemController.php';
             $controller = new ItemController();
             $controller->showAdminDashboard();
         }
-        break; // Fin del case 'admin'
+        break; 
 
     default:
         echo "Error 404 - Página no encontrada";
         break;
 } 
+
 ?> 
